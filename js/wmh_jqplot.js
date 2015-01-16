9a103c033b7d846559e383345e3d32e122c977b2
David Gibbons Windows <dgibbons@eircom.net>
2015-01-16 02:21:45 +0000

    Add target checkbox

--- js/wmh_jqplot.js
+++ js/wmh_jqplot.js
@@ -8,14 +8,19 @@ function renderTree() {
     });
 };
 
-function renderGraph(sensorlog_ids) {
-	if (sensorlog_ids[0] == "2"){
-		data = data1;
+function renderGraph(sensorlog_ids, show_target) {
+	if (sensorlog_ids[0] == "2" && show_target){
+		data = [data1, data1_target];
+		name = "Daily Steps";
+		y_label = "m2";
+	}
+	else if (sensorlog_ids[0] == "2"){
+		data = [data1];
 		name = "Daily Steps";
 		y_label = "m2";
 	}
 	else if(sensorlog_ids[0] == "3"){
-		data = data2;
+		data = [data2]; /* [data, data2] */
 		name = "Daily Active Minutes";
 		y_label = "kll";
 	}
@@ -38,18 +43,28 @@ function renderGraph(sensorlog_ids) {
 	};
 							
 	$('#webmyhealth-chart').empty();
-	var plot1 = $.jqplot('webmyhealth-chart', [data], my_options);
-	$('#sensorlog-name').empty();
-	$('#sensorlog-name').text(name);
+	var plot1 = $.jqplot('webmyhealth-chart', data, my_options);
+
 	return plot1;
 };
 
 
+function listen_show_target(plot1) {
+	$('input:checkbox').change(function(){
+		if($(this).is(':checked')){
+			plot1 = renderGraph(["2"], true);
+		}
+	});
+	return plot1
+}
+
 $(document).ready(function(){
     renderTree();
-	plot1 = renderGraph(["2"]);
+	plot1 = renderGraph(["2"], false);
+	plot1 = listen_show_target(plot1);
 });
 
 $(window).resize(function(){
 	plot1.replot( {resetAxes: true } );
 });
+

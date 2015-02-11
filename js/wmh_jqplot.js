2e1fce84a3d3ce1e17e6ee7ebeb14f8c9755f9d6
David Gibbons Windows <dgibbons@eircom.net>
2015-02-11 01:25:32 +0000

    Getting tablesorter working

--- js/wmh_jqplot.js
+++ js/wmh_jqplot.js
@@ -10,13 +10,12 @@ function renderTree() {
 	// Listen for event: tree changing
 	$('#jstree_div').on("changed.jstree", function (e, data) {
 	   graph.sensorlog_ids = data.selected;
-	   plot1 = renderGraph(graph.sensorlog_ids, graph.show_target);
+	   /^plot1 = renderGraph(graph.sensorlog_ids, graph.show_target);*/
 	   show_stat_text();
     });
 };
 
-
-function renderGraph(sensorlog_ids, show_target) {
+function get_graph_data(sensorlog_ids, show_target) {
 	if (sensorlog_ids[0] == "2" && show_target){
 		data = [data1, data1_target];
 		names = ["Daily Steps", "Daily Steps Target"];
@@ -37,6 +36,15 @@ function renderGraph(sensorlog_ids, show_target) {
 		names = ["Daily Active Minutes"];
 		y_label = "mins";
 	}
+	
+	return [data, names, y_label]
+}
+
+function renderGraph(sensorlog_ids, show_target) {
+    returned_array = get_graph_data(sensorlog_ids, show_target);
+	data = returned_array[0];
+	names = returned_array[1];
+	y_label = returned_array[2];
 
 	my_axes = {xaxis: { renderer:$.jqplot.DateAxisRenderer, label: "Date"},
 	           yaxis: { label: y_label,}}
@@ -103,84 +111,32 @@ function listen_show_target(plot1) {
 };
 
 
-var dataSet = [
-    ['Trident','Internet Explorer 4.0','Win 95+','4','X'],
-    ['Trident','Internet Explorer 5.0','Win 95+','5','C'],
-    ['Trident','Internet Explorer 5.5','Win 95+','5.5','A'],
-    ['Trident','Internet Explorer 6','Win 98+','6','A'],
-    ['Trident','Internet Explorer 7','Win XP SP2+','7','A'],
-    ['Trident','AOL browser (AOL desktop)','Win XP','6','A'],
-    ['Gecko','Firefox 1.0','Win 98+ / OSX.2+','1.7','A'],
-    ['Gecko','Firefox 1.5','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Firefox 2.0','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Firefox 3.0','Win 2k+ / OSX.3+','1.9','A'],
-    ['Gecko','Camino 1.0','OSX.2+','1.8','A'],
-    ['Gecko','Camino 1.5','OSX.3+','1.8','A'],
-    ['Gecko','Netscape 7.2','Win 95+ / Mac OS 8.6-9.2','1.7','A'],
-    ['Gecko','Netscape Browser 8','Win 98SE+','1.7','A'],
-    ['Gecko','Netscape Navigator 9','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Mozilla 1.0','Win 95+ / OSX.1+',1,'A'],
-    ['Gecko','Mozilla 1.1','Win 95+ / OSX.1+',1.1,'A'],
-    ['Gecko','Mozilla 1.2','Win 95+ / OSX.1+',1.2,'A'],
-    ['Gecko','Mozilla 1.3','Win 95+ / OSX.1+',1.3,'A'],
-    ['Gecko','Mozilla 1.4','Win 95+ / OSX.1+',1.4,'A'],
-    ['Gecko','Mozilla 1.5','Win 95+ / OSX.1+',1.5,'A'],
-    ['Gecko','Mozilla 1.6','Win 95+ / OSX.1+',1.6,'A'],
-    ['Gecko','Mozilla 1.7','Win 98+ / OSX.1+',1.7,'A'],
-    ['Gecko','Mozilla 1.8','Win 98+ / OSX.1+',1.8,'A'],
-    ['Gecko','Seamonkey 1.1','Win 98+ / OSX.2+','1.8','A'],
-    ['Gecko','Epiphany 2.20','Gnome','1.8','A'],
-    ['Webkit','Safari 1.2','OSX.3','125.5','A'],
-    ['Webkit','Safari 1.3','OSX.3','312.8','A'],
-    ['Webkit','Safari 2.0','OSX.4+','419.3','A'],
-    ['Webkit','Safari 3.0','OSX.4+','522.1','A'],
-    ['Webkit','OmniWeb 5.5','OSX.4+','420','A'],
-    ['Webkit','iPod Touch / iPhone','iPod','420.1','A'],
-    ['Webkit','S60','S60','413','A'],
-    ['Presto','Opera 7.0','Win 95+ / OSX.1+','-','A'],
-    ['Presto','Opera 7.5','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 8.0','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 8.5','Win 95+ / OSX.2+','-','A'],
-    ['Presto','Opera 9.0','Win 95+ / OSX.3+','-','A'],
-    ['Presto','Opera 9.2','Win 88+ / OSX.3+','-','A'],
-    ['Presto','Opera 9.5','Win 88+ / OSX.3+','-','A'],
-    ['Presto','Opera for Wii','Wii','-','A'],
-    ['Presto','Nokia N800','N800','-','A'],
-    ['Presto','Nintendo DS browser','Nintendo DS','8.5','C/A<sup>1</sup>'],
-    ['KHTML','Konqureror 3.1','KDE 3.1','3.1','C'],
-    ['KHTML','Konqureror 3.3','KDE 3.3','3.3','A'],
-    ['KHTML','Konqureror 3.5','KDE 3.5','3.5','A'],
-    ['Tasman','Internet Explorer 4.5','Mac OS 8-9','-','X'],
-    ['Tasman','Internet Explorer 5.1','Mac OS 7.6-9','1','C'],
-    ['Tasman','Internet Explorer 5.2','Mac OS 8-X','1','C'],
-    ['Misc','NetFront 3.1','Embedded devices','-','C'],
-    ['Misc','NetFront 3.4','Embedded devices','-','A'],
-    ['Misc','Dillo 0.8','Embedded devices','-','X'],
-    ['Misc','Links','Text only','-','X'],
-    ['Misc','Lynx','Text only','-','X'],
-    ['Misc','IE Mobile','Windows Mobile 6','-','C'],
-    ['Misc','PSP browser','PSP','-','C'],
-    ['Other browsers','All others','-','-','U']
-];
 
-function renderTable() {
+function renderTable(sensorlog_ids, show_target) {
+	returned_array = get_graph_data(sensorlog_ids, show_target);
+	data = returned_array[0];
+	names = returned_array[1];
+	y_label = returned_array[2];
+	
 	$('#webmyhealth-chart').empty();
     $('#webmyhealth-chart').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example" style="max-width:600px"></table>' );
- 
-    $('#example').dataTable( {
-        "data": data1,
-        "columns": [
-            { "title": "Date and Time" },
-			{ "title": "Day" },
-            { "title": "Value" },
-            /*{ "title": "Platform" },
-            { "title": "Version", "class": "center" },
-            { "title": "Grade", "class": "center" }*/
-        ],
-		"ColReorder": {
-            "Order": [0, 2, 1]
-        },
-    } ); 
+    $('#example').html( '<thead><tr><th>Date and Time</th><th>' + names[0] + ' (' + y_label + ')' + '</th></tr></thead>' );
+	$('#example').append( '<tbody>' );
+	for (i = 0; i < data[0].length; i++){
+		$('#example').append('<tr><td>' + data[0][i][0] + '</td><td>' + data[0][i][1] + '</td><tr>');
+		}
+	$('#example').append( '</tbody>' );
+	
+    $("#example").tablesorter({
+		 dateFormat : "yyyy-mm-dd", // ?? set the default date format
+		 
+		  headers: {
+             0: { sorter: "shortDate" }, //, dateFormat will parsed as the default above)
+          },
+		 
+	    // Initially sort on the first column, order descending
+		sortList: [[0,1]]
+	});
 };
 
 function activate_chosen() {
@@ -191,10 +147,10 @@ function activate_chosen() {
 $(document).ready(function(){
     renderTree();
 	activate_chosen();
-	plot1 = renderGraph(graph.sensorlog_ids, false); 
-	plot1 = listen_show_target(plot1);
+	/*plot1 = renderGraph(graph.sensorlog_ids, false); 
+	plot1 = listen_show_target(plot1);*/
 	
-	/*renderTable();*/
+	renderTable(graph.sensorlog_ids, false);
 });
 
 $(window).resize(function(){

16ba3a303c4ee9ab31c16205fb796198fa60ee16
David Gibbons Windows <dgibbons@eircom.net>
2015-03-12 03:18:54 +0000

    Work from week before Patricks Day

---
+++ js/banner.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+jQuery(document).ready(function($) {
+	$('#my-slideshow').bjqs({
+		'height' : 320,
+		'width' : 620,
+		'animspeed' : 8000,
+		'responsive' : true
+	});
+});
